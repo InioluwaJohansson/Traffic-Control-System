8b0041c12476ab53abda98cd66c979477cd692b4
Inioluwa Johansson <inioluwa.makinde10@gmail.com>
2025-07-19 12:51:48 -0400

    new

--- Entities/Violation.cs
+++ Entities/Violation.cs
@@ -7,5 +7,6 @@ public class Violation : AuditableEntity
     public int LaneId { get; set; }
     public string VehicleNumber { get; set; }
     public int Speed { get; set; }
+    public string ImageUrl { get; set; }
     public DateTime ViolationTime { get; set; }
 }
