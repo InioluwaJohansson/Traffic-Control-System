bbaa19c8c661906bf4d7277e954307039c3f6ac6
Inioluwa Johansson <inioluwa.makinde10@gmail.com>
2025-07-31 14:50:22 +0100

    new changes

---
+++ Migrations/20250731134501_NewUpdatedTrafficMigration4.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Traffic_Control_System.Migrations
+{
+    public partial class NewUpdatedTrafficMigration4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
